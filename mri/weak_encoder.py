196c3178b0431a84f26546277e2e804e9029c717
PierreAuriau <p.auriau@hotmail.fr>
2024-02-06 17:05:43 +0100

    add save hyperparameters and main functions

--- mri/weak_encoder.py
+++ mri/weak_encoder.py
@@ -10,6 +10,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import pickle
+import json
 import torch
 import torch.nn as nn
 from torch.optim import Adam
@@ -21,7 +22,7 @@ from sklearn.linear_model import LogisticRegression, Ridge
 # project import
 from loss import align_loss, uniform_loss, norm
 from datamanager import ClinicalDataManager
-from logs import setup_logging, save_hyperparameters, History
+from logs import setup_logging, History
 from encoder import Encoder
 
 logger = logging.getLogger("WeakEncoder")
@@ -37,10 +38,11 @@ class WeakEncoder:
 
     def train(self, chkpt_dir, exp_name, dataset, nb_epochs, 
               data_augmentation, nb_epochs_per_saving=10):
+        
         # loader
         manager = ClinicalDataManager(root="/neurospin/psy_sbox/analyses/2023_pauriau_sepmod/data/root", 
                                       db=dataset, preproc="skeleton", labels=None, 
-                                      batch_size=8, two_views=True,
+                                      batch_size=32, two_views=True, data_augmentation=data_augmentation,
                                       num_workers=8, pin_memory=True)
         loader = manager.get_dataloader(train=True, validation=False)
         nb_batch = len(loader.train)
@@ -50,7 +52,8 @@ class WeakEncoder:
         # prepare attributes
         self.checkpointdir = chkpt_dir
         self.exp_name = exp_name
-        self.history = History(name="Train_WeakEncoder", chkpt_dir=chkpt_dir)
+        self.history = History(name=f"Train_WeakEncoder_exp-{exp_name}", chkpt_dir=chkpt_dir)
+        self.save_hyperparameters(nb_epochs=nb_epochs, dataset=dataset, data_augmentation=data_augmentation)
 
         # train model
         self.weak_encoder.to(self.device)
@@ -72,7 +75,7 @@ class WeakEncoder:
             pbar.close()
             self.history.log(epoch=epoch, train_loss=train_loss)
             self.history.reduce(reduce_fx="sum")
-        if (epoch % nb_epochs_per_saving == 0 or epoch == nb_epochs-1) and epoch > 0:
+        if (epoch % nb_epochs_per_saving == 0 or epoch == nb_epochs-1):
             self.history.summary()
             self.save_checkpoint(epoch=epoch,
                                  n_embedding=self.n_embedding,
@@ -97,7 +100,7 @@ class WeakEncoder:
                                       num_workers=8, pin_memory=True)
         self.exp_name = exp_name
         self.checkpointdir = chkpt_dir
-        self.history = History(name="Test_StrongEncoder", chkpt_dir=chkpt_dir)
+        self.history = History(name=f"Test_StrongEncoder_exp-{exp_name}", chkpt_dir=chkpt_dir)
 
         for epoch in list_epochs:
             self.looad_checkpoint(epoch=epoch)
@@ -184,6 +187,17 @@ class WeakEncoder:
     
     def get_representatinon_name(self, epoch):
         return f"Representations_WeakEncoder_exp-{self.exp_name}_ep-{epoch}.pkl"
+    
+    def save_hyperparameters(self, **kwargs):
+        filename = f"WeakEncoder_exp-{self.exp_name}_hyperparameters.json"
+        hyperparameters = {"backbone": self.backbone, "n_embeddings": self.n_embedding,
+                           "bactch_size": 32, "preproc": "skeleton", "lr": 1e-4, 
+                           "weight_decay": 5e-5, **kwargs}
+        for k,v in hyperparameters.items():
+            logger.debug(f"Key: {k} | type : {type(v)}")
+            logger.debug(f"Value : {v}")
+        with open(os.path.join(self.checkpointdir, filename), "w") as f:
+            json.dump(hyperparameters, f)
         
     @staticmethod
     def loss_fn(z_1, z_2):
@@ -192,7 +206,7 @@ class WeakEncoder:
         return weak_align_loss + weak_uniform_loss
     
     def configure_optimizers(self):
-        optimizer = Adam(self.weak_encoder.parameters(), lr=1e-4)
+        optimizer = Adam(self.weak_encoder.parameters(), lr=1e-4, weight_decay=5e-5)
         return optimizer
     
     def save_checkpoint(self, epoch, **kwargs):
@@ -213,17 +227,55 @@ class WeakEncoder:
 
 
 def parse_args(argv):
-    parser = argparse.Argparse()
-    parser.add_argument("--backbone", requires=True, )
-    parser.add_argument("--latent_dim", requires=True)
-    parser.add_argument("--chkpt_dir", requires=True) 
-    parser.add_argument("--exp_name", requires=True)
-    parser.add_argument("--dataset", requires=True)
-    parser.add_argument("--nb_epochs", default=50)
-    parser.add_argument("--data_augmentation")
-    parser.add_argument("--train")
-    parser.add_argument("--test")
-    parser.add_argument("--verbose")
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--backbone", required=True, choices=["resnet18", "densenet121", "alexnet"],
+                        help="Architecture of the neural network.")
+    parser.add_argument("--latent_dim", required=True, type=int,
+                        help="Dimenseion of the latent space.")
+    parser.add_argument("--chkpt_dir", required=True, type=str,
+                        help="Path to the folder where results and models are saved.") 
+    parser.add_argument("--exp_name", required=True, type=str,
+                        help="Experience name.")
+    parser.add_argument("--dataset", required=True, type=str, choices=["asd", "bd", "scz"],
+                        help="Dataset on which the model is trained.")
+    parser.add_argument("--nb_epochs", type=int, default=50,
+                        help="Number of training epochs. Default is 50.")
+    parser.add_argument("--data_augmentation", type=str, default="Cutout", nargs="+",
+                        help="Data augmentation for model training. Default is: Cutout.")
+    parser.add_argument("--labels", type=str, nargs="+", choices=["diagnosis", "age", "site", "sex", "tiv"],
+                        help="Labels on which the model will be tested.")
+    parser.add_argument("--epochs_to_test", type=int, nargs="+", default=49,
+                        help="Epoch at which the model will be tested.")
+    parser.add_argument("--train", action="store_true",
+                        help="Train model")
+    parser.add_argument("--test", action="store_true",
+                        help="Test the model")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Activate verbosity mode")
+
+    args = parser.parse_args(argv)
+    
+    return args
+
+def main(argv):
+    args = parse_args(argv)
+
+    # Create saving directory
+    os.makedirs(args.chkpt_dir, exist_ok=True)
+
+    # Setup Logging
+    setup_logging(level="debug" if args.verbose else "info",
+                  logfile=os.path.join(args.chkpt_dir, f"{args.exp_name}.log"))
+    
+    logger.info(f"Checkpoint directory : {args.chkpt_dir}")
+
+    model = WeakEncoder(backbone=args.backbone, latent_dim=args.latent_dim)
+
+    if args.train:
+        model.train(chkpt_dir=args.chkpt_dir, exp_name=args.exp_name, dataset=args.dataset,
+                    nb_epochs=args.nb_epochs, data_augmentation=args.data_augmentation)
+    if args.test:
+        model.test(chkpt_dir=args.chkpt_dir, exp_name=args.exp_name, dataset=args.dataset,
+                   labels=args.labels, list_epochs=args.epoch_to_test)
 
 if __name__ == "__main__":
-    pass
+    main(sys.argv[1:])
