b9be9b69cb126c2fcad3ed76a84dfe366ce060c1
PierreAuriau <p.auriau@hotmail.fr>
2024-08-29 16:00:01 +0200

    update to match sepmod standards

--- photo_sketching/vae.py
+++ photo_sketching/vae.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 from tqdm import tqdm
 import logging
+import json
 
 import torch
 import torch.nn as nn
@@ -17,17 +18,30 @@ from encoders import Encoder, Conv7Encoder
 from decoders import Decoder, Conv7Decoder
 from loggers import TrainLogger
 
+logging.setLoggerClass(TrainLogger)
+
 
 class VAE(nn.Module):
 
-    def __init__(self, input_channels, latent_dim=256):
+    def __init__(self, input_channels, latent_dim=256, beta=1.0, nb_layers=None):
         super().__init__()
-        self.encoder = Conv7Encoder(input_channels=input_channels,
-                                    latent_dim=latent_dim)
-        self.decoder = Conv7Decoder(input_channels=input_channels,
-                                    latent_dim=latent_dim)
+        if nb_layers == 7:
+            self.encoder = Conv7Encoder(input_channels=input_channels,
+                                        latent_dim=latent_dim)
+            self.decoder = Conv7Decoder(input_channels=input_channels,
+                                        latent_dim=latent_dim)
+        else:
+            self.encoder = Encoder(input_channels=input_channels,
+                                        latent_dim=latent_dim)
+            self.decoder = Decoder(input_channels=input_channels,
+                                        latent_dim=latent_dim)
         """self.scale = nn.Parameter(torch.tensor([0.0]))"""
+        self.nb_layers = 7 if nb_layers == 7 else 4
+        self.beta = beta # FIXME: in init or in fit method ?
+        self.latent_dim = latent_dim
         self.logger = logging.getLogger("vae")
+        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.logger.info(f"Device used : {self.device}")
         
     def reparamatrize(self, mean, logvar):
         std = torch.exp(logvar/2)
@@ -51,7 +65,7 @@ class VAE(nn.Module):
     def loss_fn(self, inputs, outputs, mean, logvar):
         kl_loss = -0.5 * torch.sum(1 + logvar - mean.pow(2) - logvar.exp())
         rec_loss = F.mse_loss(outputs, inputs, reduction="sum")
-        return kl_loss + rec_loss
+        return rec_loss, kl_loss
 
     def forward(self, inputs):
         mean, logvar = self.encoder(inputs)
@@ -71,58 +85,62 @@ class VAE(nn.Module):
     def fit(self, train_loader, val_loader, nb_epochs, 
             modality, chkpt_dir, **kwargs_optimizer):
         
-        self.train_logger = TrainLogger("trainlogger")
-        self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
-        self.logger.info(f"Device used : {self.device}")
         self.optimizer = self.configure_optimizers(**kwargs_optimizer)
         self.lr_scheduler = None
+        self.save_hyperparameters(chkpt_dir)
         self.encoder = self.encoder.to(self.device)
         self.decoder = self.decoder.to(self.device)
+        self.logger.reset_history()
         best_epoch, best_loss = 0, np.inf
         for epoch in range(nb_epochs):
-            self.train_logger.step()
-            self.train_logger.store(epoch=epoch)
             self.logger.info(f"Epoch: {epoch}")
             train_loss, val_loss = 0, 0
-    
+
             self.encoder = self.encoder.train()
             self.decoder = self.decoder.train()
+            self.logger.step()
             for batch in tqdm(train_loader, desc="train"):
                 inputs = batch[modality].to(self.device)
                 loss = self.training_step(inputs)
                 train_loss += loss
+            self.logger.reduce(reduce_fx="sum")
+            self.logger.store(epoch=epoch, set="train", loss=train_loss)
             
             self.encoder = self.encoder.eval()
             self.decoder = self.decoder.eval()
+            self.logger.step()
             for batch in tqdm(val_loader, desc="val"):
                 inputs = batch[modality].to(self.device)
                 val_loss += self.valid_step(inputs)
+            self.logger.reduce(reduce_fx="sum")
+            self.logger.store(epoch=epoch, set="validation", loss=val_loss)
 
-            self.train_logger.reduce(reduce_fx="sum")
-            self.train_logger.store(train_loss=train_loss, val_loss=val_loss)
             if val_loss <= best_loss:
+                self.logger.info(f"Validation loss improvement : {best_loss:.2g} --> {val_loss:.2g}")
                 best_loss = val_loss
                 best_epoch = epoch
-                self.logger.info(self.train_logger.summary())
                 self.save_chkpt(os.path.join(chkpt_dir,
                                              f'vae_mod-{modality}_ep-best.pth'))
             if epoch % 10 == 0:
-                self.logger.info(self.train_logger.summary())
+                self.logger.info(f"Train loss: {train_loss:.2g} - Val loss: {val_loss:.2g}")
+                self.logger.info(f"Training duration: {self.logger.get_duration()}")
                 self.save_chkpt(os.path.join(chkpt_dir,
                                              f'vae_mod-{modality}_ep-{epoch}.pth'))
         
-        self.train_logger.save(chkpt_dir)
+        self.logger.save(chkpt_dir, filename="_train")
         self.save_chkpt(os.path.join(chkpt_dir,
                                      f'vae_mod-{modality}_ep-{epoch}.pth'))
-        self.logger.info(f"End of training: {self.train_logger.get_duration()}")
-        self.logger.info(f"Best epoch: {best_epoch} --> best loss : {val_loss}")
+        self.logger.info(f"End of training: {self.logger.get_duration()}")
+        self.logger.info(f"Best epoch: {best_epoch} - best val loss: {val_loss}")
 
     def training_step(self, inputs):
         self.optimizer.zero_grad()
         mean, logvar = self.encoder(inputs)
         z = self.reparamatrize(mean, logvar)
         outputs = self.decoder(z)
-        loss = self.loss_fn(inputs, outputs, mean, logvar)
+        rec_loss, kl_loss = self.loss_fn(inputs, outputs, mean, logvar)
+        self.logger.store(reconstruction_loss=rec_loss.item(), kl_divergence=kl_loss.item())
+        loss = rec_loss + self.beta*kl_loss
         loss.backward()
         self.optimizer.step()
         if self.lr_scheduler:
@@ -134,21 +152,28 @@ class VAE(nn.Module):
             mean, logvar = self.encoder(inputs)
             z = self.reparamatrize(mean, logvar)
             outputs = self.decoder(z)
-            loss = self.loss_fn(inputs, outputs, mean, logvar)
+            rec_loss, kl_loss = self.loss_fn(inputs, outputs, mean, logvar)
+            self.logger.store(reconstruction_loss=rec_loss.item(), kl_divergence=kl_loss.item())
+            loss = rec_loss + self.beta*kl_loss
         return loss.item()
 
     def test(self, loader, epoch, modality, chkpt_dir):
         self.load_chkpt(os.path.join(chkpt_dir,
                                 f'vae_mod-{modality}_ep-{epoch}.pth'))
-        reconstructions = self.get_reconstructions(loader)
+        reconstructions = self.get_reconstructions(loader, modality)
         test_loss = 0
+        self.logger.step()
         for batch in tqdm(loader, desc="test"):
             inputs = batch[modality].to(self.device)
             test_loss += self.valid_step(inputs)
         self.logger.info(f"Test loss : {test_loss:.2g}")
-        np.save(os.path.join(chkpt_dir, f"reconstructions_ep-{epoch}.npy"), reconstructions)
+        self.logger.reduce(reduce_fx="sum")
+        self.logger.store(epoch=epoch, set="test", loss=test_loss)
+        self.logger.save(chkpt_dir, filename="_test")
+        filename = f"reconstructions_set-test_ep-{epoch}.npy"
+        np.save(os.path.join(chkpt_dir, filename), reconstructions)
 
-    def get_reconstructions(self, loader):
+    def get_reconstructions(self, loader, modality):
         self.encoder.eval()
         self.decoder.eval()
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
@@ -157,15 +182,25 @@ class VAE(nn.Module):
         outputs = []
         with torch.no_grad():
             for batch in loader:
-                inputs = batch[modality].to(model.device)
+                inputs = batch[modality].to(self.device)
                 outputs.extend(self(inputs).cpu().numpy())
         outputs = np.asarray(outputs)
         # normalize outputs
-        outputs = (outputs - outputs.min(axis=0)) / (outputs.max(axis=0) - outputs.min(axis=0)) * 255
+        if self.nb_layers == 7: # with sigmoid
+            outputs = outputs * 255
+        else: # no sigmoid
+            outputs = (outputs - outputs.min(axis=0)) / (outputs.max(axis=0) - outputs.min(axis=0)) * 255
         outputs = outputs.astype(int)
         # put channels in last
         outputs = np.moveaxis(outputs, 1, -1)
         return outputs
+    
+    def save_hyperparameters(self, chkpt_dir):
+        hp = {"latent_dim": self.latent_dim,
+              "nb_layers": self.nb_layers,
+              "beta": self.beta}
+        with open(os.path.join(chkpt_dir, "hyperparameters.json"), "w") as f:
+            json.dump(hp, f)
 
     def save_chkpt(self, filename):
         torch.save({"encoder": self.encoder.state_dict(),
