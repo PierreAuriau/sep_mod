ae9f156b7dc8a7af8b36d862590c9d5cd15ac3af
PierreAuriau <p.auriau@hotmail.fr>
2025-03-18 12:13:30 +0100

    add pytorchvae architectures

--- morpho_mnist/sepmod.py
+++ morpho_mnist/sepmod.py
@@ -16,8 +16,8 @@ import torch.optim as optim
 import torch.nn.functional as F
 from torch.cuda.amp import GradScaler, autocast
 
-from encoders import Encoder, Conv6Encoder
-from decoders import Decoder, Conv6Decoder
+from encoders import Encoder, Conv6Encoder, PytorchEncoder
+from decoders import Decoder, Conv6Decoder, PytorchDecoder
 from classifiers import Classifier
 from loggers import TrainLogger
 
@@ -37,27 +37,27 @@ logging.setLoggerClass(TrainLogger)
 
 class SepMod(nn.Module):
 
-    def __init__(self, latent_dim=16, 
+    def __init__(self, latent_dim=16,
                  tc_loss_between_specifics=False,
                  tc_loss_between_shared_and_specific=True, 
-                 kl_loss_between_shared=True):
+                 loss_between_shared="kl"):
         super().__init__()
         self.encoders = nn.ModuleDict({
-            "skeleton": Encoder(latent_dim=latent_dim//2),
-            "image": Encoder(latent_dim=latent_dim//2),
-            "shared": Encoder(latent_dim=latent_dim//2)
+            "skeleton": PytorchEncoder(latent_dim=latent_dim//2),
+            "image": PytorchEncoder(latent_dim=latent_dim//2),
+            "shared": PytorchEncoder(latent_dim=latent_dim//2)
         })
         self.decoders = nn.ModuleDict({
-            "skeleton": Decoder(latent_dim=latent_dim),
-            "image": Decoder(latent_dim=latent_dim)
+            "skeleton": PytorchDecoder(latent_dim=latent_dim),
+            "image": PytorchDecoder(latent_dim=latent_dim)
         })        
 
         self.discriminators = nn.ModuleDict()
         if tc_loss_between_shared_and_specific:
             self.discriminators["skeleton"] = Classifier(latent_dim=latent_dim)
             self.discriminators["image"] = Classifier(latent_dim=latent_dim)
-        # if tc_loss_between_specifics:
-        #     self.discriminators["specific"] = Classifier(latent_dim=latent_dim)
+        if tc_loss_between_specifics:
+             self.discriminators["specific"] = Classifier(latent_dim=latent_dim)
         self.logger = logging.getLogger("sepmod")
         
         # Hyperparameters
@@ -65,10 +65,10 @@ class SepMod(nn.Module):
         self.specific_slice = slice(0, (latent_dim//2)) # see get_embeddings, train/valid step
         self.shared_slice = slice((latent_dim//2), latent_dim) # see get_embeddings, train/valid step
         self.tc_loss_between_specifics = tc_loss_between_specifics
-        self.kl_loss_between_shared = kl_loss_between_shared
+        self.loss_between_shared = loss_between_shared
         self.tc_loss_between_shared_and_specific = tc_loss_between_shared_and_specific
         self.betas = defaultdict(lambda: 1.0)
-
+        self.gammas = torch.FloatTensor([10 ** x for x in range(-6, 7, 1)])
         self.device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
         self.logger.info(f"Device used : {self.device}")
         if not (tc_loss_between_shared_and_specific or tc_loss_between_shared_and_specific):
@@ -89,8 +89,28 @@ class SepMod(nn.Module):
             kl_div = -0.5 * torch.sum(1 + logvar - logvar_  - (logvar.exp() + (mean - mean_).pow(2)) / (2*logvar_.exp()))
         return kl_div
 
+    def mmd(self, x, y):
+        gammas = self.gammas.to(self.device)
+        cost = torch.mean(self.gram_matrix(x, x, gammas=gammas)).to(self.device)
+        cost += torch.mean(self.gram_matrix(y, y, gammas=gammas)).to(self.device)
+        cost -= 2 * torch.mean(self.gram_matrix(x, y, gammas=gammas)).to(self.device)
+
+        if cost < 0:
+            return torch.tensor(0).to(self.device)
+        return cost
+
+    @staticmethod
+    def gram_matrix(x, y, gammas):
+        gammas = gammas.unsqueeze(1)
+        pairwise_distances = torch.cdist(x, y, p=2.0)
+
+        pairwise_distances_sq = torch.square(pairwise_distances)
+        tmp = torch.matmul(gammas, torch.reshape(pairwise_distances_sq, (1, -1)))
+        tmp = torch.reshape(torch.sum(torch.exp(-tmp), 0), pairwise_distances_sq.shape)
+        return tmp
+
     
-    def loss_fn(self, inputs, outputs, mean_specific, logvar_specific, mean_shared, logvar_shared, joint_predictions):
+    def loss_fn(self, inputs, outputs, mean_specific, logvar_specific, mean_shared, logvar_shared, joint_predictions, z):
         kl_loss = 0
         for mod in ("image", "skeleton"):
             kl_loss += self.kl_divergence(mean_specific[mod], logvar_specific[mod])
@@ -102,11 +122,13 @@ class SepMod(nn.Module):
         if self.tc_loss_between_shared_and_specific:
             for mod in ("image", "skeleton"):
                 tc_loss += torch.log(joint_predictions[mod] / (1 - joint_predictions[mod])).sum()
-        # if self.tc_loss_between_specifics:
-        #     tc_loss += F.relu(torch.log(joint_predictions["specific"] / (1 - joint_predictions["specific"]))).sum()
+        if self.tc_loss_between_specifics:
+            tc_loss += F.relu(torch.log(joint_predictions["specific"] / (1 - joint_predictions["specific"]))).sum()
         alg_loss = 0
-        if self.kl_loss_between_shared:
+        if self.loss_between_shared == "kl":
             alg_loss = self.kl_divergence(mean_shared["image"], logvar_shared["image"], mean_shared["skeleton"], logvar_shared["skeleton"])
+        elif self.loss_between_shared == "mmd":
+            alg_loss = self.mmd(z["image"][self.shared_slice], z["skeleton"][self.shared_slice])
         return kl_loss, rec_loss, tc_loss, alg_loss
 
     def forward(self, inputs):
@@ -198,20 +220,20 @@ class SepMod(nn.Module):
                                                            torch.zeros_like(product_of_marginals_predictions[:, 0])), dim=0)
                         discriminator_loss += F.binary_cross_entropy_with_logits(discriminator_predictions, discriminator_targets, reduction="sum") # FIXME: remove logits ?
             
-            # if self.tc_loss_between_specifics:
-            #     z_specific = torch.cat((z["image"][:, (self.latent_dim//2):] ,
-            #                             z["skeleton"][:, (self.latent_dim//2):]), 
-            #                             dim=1)
-            #     with autocast(dtype=torch.float16):
-            #         joint_predictions = self.discriminators["specific"](z_specific)
-            #         product_of_marginals_predictions = self.discriminators["specific"]((torch.cat((z_specific[:, (self.latent_dim//2):],
-            #                                                                                 z_specific[torch.randperm(z.size(0)), :(self.latent_dim//2)]), 
-            #                                                                                 dim=1)))
-            #         discriminator_predictions = torch.cat((joint_predictions[:, 0], 
-            #                                                 product_of_marginals_predictions[:, 0]), dim=0)
-            #         discriminator_targets = torch.cat((torch.ones_like(joint_predictions[:, 0]), 
-            #                                             torch.zeros_like(product_of_marginals_predictions[:, 0])), dim=0)
-            #         discriminator_loss += F.binary_cross_entropy_with_logits(discriminator_predictions, discriminator_targets, reduction="sum")                                                                          
+            if self.tc_loss_between_specifics:
+                z_specific = torch.cat((z["image"][:, self.specific_slice] ,
+                                        z["skeleton"][:, self.specific_slice]), 
+                                        dim=1)
+                with autocast(dtype=torch.float16):
+                    joint_predictions = self.discriminators["specific"](z_specific)
+                    product_of_marginals_predictions = self.discriminators["specific"]((torch.cat((z_specific[:, (self.latent_dim//2):],
+                                                                                                   z_specific[torch.randperm(z_specific.size(0)), :(self.latent_dim//2)]), 
+                                                                                                   dim=1)))
+                    discriminator_predictions = torch.cat((joint_predictions[:, 0], 
+                                                            product_of_marginals_predictions[:, 0]), dim=0)
+                    discriminator_targets = torch.cat((torch.ones_like(joint_predictions[:, 0]), 
+                                                       torch.zeros_like(product_of_marginals_predictions[:, 0])), dim=0)
+                    discriminator_loss += F.binary_cross_entropy_with_logits(discriminator_predictions, discriminator_targets, reduction="sum")                                                                          
             self.scaler.scale(discriminator_loss).backward()
             self.scaler.step(self.optimizer_discriminators)
             self.logger.store(discriminator_loss=discriminator_loss.item())
@@ -232,12 +254,12 @@ class SepMod(nn.Module):
                 outputs[mod] = self.decoders[mod](z[mod])
                 if self.tc_loss_between_shared_and_specific:
                     joint_predictions[mod] = self.discriminators[mod](z[mod])
-            # if self.tc_loss_between_specifics:
-            #     z_specific = torch.cat((z["image"][:, self.specific_slice] ,
-            #                             z["skeleton"][:, self.specific_slice]), 
-            #                         dim=1)
-            #     joint_predictions["specific"] = self.discriminators["specific"](z_specific)
-            kl_loss, rec_loss, tc_loss, alg_loss = self.loss_fn(inputs, outputs, mean_specific, logvar_specific, mean_shared, logvar_shared, joint_predictions)
+            if self.tc_loss_between_specifics:
+                z_specific = torch.cat((z["image"][:, self.specific_slice] ,
+                                        z["skeleton"][:, self.specific_slice]), 
+                                        dim=1)
+                joint_predictions["specific"] = self.discriminators["specific"](z_specific)
+            kl_loss, rec_loss, tc_loss, alg_loss = self.loss_fn(inputs, outputs, mean_specific, logvar_specific, mean_shared, logvar_shared, joint_predictions, z)
             # if torch.isnan(tc_loss):
             #     import pdb
             #     pdb.set_trace(header="Nan")
@@ -249,9 +271,12 @@ class SepMod(nn.Module):
             if self.tc_loss_between_shared_and_specific or self.tc_loss_between_specifics:
                 loss += self.betas["total_correlation"]*tc_loss
                 self.logger.store(total_correlation_loss=tc_loss.item())
-            if self.kl_loss_between_shared:
+                if np.isnan(tc_loss.item()):
+                    raise ValueError("Nan in the total correlatoin loss !")
+            if self.loss_between_shared is not None:
                 loss += self.betas["alignement"]*alg_loss
                 self.logger.store(alignement_loss=alg_loss.item())
+
         self.scaler.scale(loss).backward()
         self.scaler.step(self.optimizer_vae)
         self.scaler.update()            
@@ -272,12 +297,12 @@ class SepMod(nn.Module):
                 outputs[mod] = self.decoders[mod](z[mod])
                 if self.tc_loss_between_shared_and_specific:
                     joint_predictions[mod] = self.discriminators[mod](z[mod])
-        # if self.tc_loss_between_specifics:
-        #     z_specific = torch.cat((z["image"][:, (self.latent_dim//2):] ,
-        #                             z["skeleton"][:, (self.latent_dim//2):]), 
-        #                         dim=1)
-        #     joint_predictions["specific"] = self.discriminators["specific"](z_specific)
-            kl_loss, rec_loss, tc_loss, alg_loss = self.loss_fn(inputs, outputs, mean_specific, logvar_specific, mean_shared, logvar_shared, joint_predictions)
+            if self.tc_loss_between_specifics:
+                z_specific = torch.cat((z["image"][:, self.specific_slice] ,
+                                        z["skeleton"][:, self.specific_slice]), 
+                                    dim=1)
+                joint_predictions["specific"] = self.discriminators["specific"](z_specific)
+            kl_loss, rec_loss, tc_loss, alg_loss = self.loss_fn(inputs, outputs, mean_specific, logvar_specific, mean_shared, logvar_shared, joint_predictions, z)
             self.logger.store(kl_loss=kl_loss.item(), reconstruction_loss=rec_loss.item())
             loss = kl_loss + rec_loss
             if self.tc_loss_between_shared_and_specific or self.tc_loss_between_specifics:
@@ -319,32 +344,38 @@ class SepMod(nn.Module):
                 outputs = linear_probe
         return 0
 
-    def get_reconstructions(self, loader):
+    def get_reconstructions(self, loader, raw=False, return_loss=False):
         self.eval()
         reconstructions = {"image": [], "skeleton": []}
+        test_loss = {"image": [], "skeleton": []}
         with torch.no_grad():
             for inputs in loader:
                 for mod in ("image", "skeleton"):
                     inputs[mod] = inputs[mod].to(self.device)
                 outputs = self(inputs)
                 for mod in ("image", "skeleton"):
+                    test_loss[mod] += F.mse_loss(inputs[mod], outputs[mod], reduction="sum").item()
                     reconstructions[mod].extend(outputs[mod].cpu().numpy())
+        self.logger.info(f"Reconstruction error: {test_loss}")
         for mod in ("image", "skeleton"):
             outputs = np.array(reconstructions[mod])
-            # normalize outputs
-            # outputs = (outputs - outputs.min(axis=0)) / (outputs.max(axis=0) - outputs.min(axis=0)) * 255
-            outputs = (np.clip(outputs, -1, 1)*0.5 + 0.5) * 255
-            outputs = outputs.astype(int)
+            if not raw:
+                # normalize outputs
+                # outputs = (outputs - outputs.min(axis=0)) / (outputs.max(axis=0) - outputs.min(axis=0)) * 255
+                outputs = (np.clip(outputs, -1, 1)*0.5 + 0.5)
+                outputs = outputs.astype(int)
             # put channels in last
             outputs = np.moveaxis(outputs, 1, -1)
             reconstructions[mod] = outputs
+        if return_loss:
+            return reconstructions, test_loss
         return reconstructions
     
     def save_hyperparameters(self, chkpt_dir):
         hp = {"latent_dim": self.latent_dim,
               "tc_loss_between_shared_and_specific": self.tc_loss_between_shared_and_specific,
               "tc_loss_between_specifics": self.tc_loss_between_specifics,
-              "kl_loss_between_shared": self.kl_loss_between_shared}
+              "loss_between_shared": self.loss_between_shared}
         for key, value in self.betas.items():
             hp[f"beta_{key}"] = value
         with open(os.path.join(chkpt_dir, "hyperparameters.json"), "w") as f:
